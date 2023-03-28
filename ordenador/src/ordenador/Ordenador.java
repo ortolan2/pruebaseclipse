0e2f156dd1b7c9ac688396c1bec0b2f6eede6d55
ortolan2 <126875811+ortolan2@users.noreply.github.com>
2023-03-28 10:32:02 +0200

    quitamos comentarios de los constructores

--- ordenador/src/ordenador/Ordenador.java
+++ ordenador/src/ordenador/Ordenador.java
@@ -11,27 +11,19 @@ public class Ordenador {
 	 * se informa el modelo del ordenador
 	 * variable almacena el precio total del ordenador
 	 * variable que almacena el tamaño de la memoria
-	 * variable para almacenar donde se encuentra el equipo
 	 */
 	
 	public String modelo;
 	public double precio;
 	public int memoria;
 	
-	public String ubicacionAula;
 	
-	/**
-	 * constructor vacio de la clase ordenador
-	 */
+	
+	
 	public Ordenador() {
 		
 	}
-	/**
-	 * constructor de la clase ordenador
-	 * @param modelo: se informa el modelo del ordenador
-	 * @param precio: variable almacena el precio total del ordenador
-	 * @param memoria: variable que almacena el tamaño de la memoria
-	 */
+	
 	public Ordenador(String modelo, double precio, int memoria) {
 		this.modelo = modelo;
 		this.precio = precio;
