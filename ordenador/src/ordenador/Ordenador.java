99fd5e2f65e5f0c0e8dd87003f2fcc4a94aa8e3d
ortolan2 <f.garzon@fp.safavalladolid.com>
2023-03-28 10:03:21 +0200

    nuevo version con geters and seters

--- ordenador/src/ordenador/Ordenador.java
+++ ordenador/src/ordenador/Ordenador.java
@@ -34,6 +34,44 @@ public class Ordenador {
 		this.precio = precio;
 		this.memoria = memoria;
 	}
+	/**
+	 * @return the modelo
+	 */
+	public String getModelo() {
+		return modelo;
+	}
+	/**
+	 * @param modelo the modelo to set
+	 */
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	/**
+	 * @return the precio
+	 */
+	public double getPrecio() {
+		return precio;
+	}
+	/**
+	 * @param precio the precio to set
+	 */
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	/**
+	 * @return the memoria
+	 */
+	public int getMemoria() {
+		return memoria;
+	}
+	/**
+	 * @param memoria the memoria to set
+	 */
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+	
+	
 	
 	
 }
